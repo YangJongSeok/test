a600ac0261482afd10b26f3345191c121dfdfca5
jsyang <marymet@naver.com>
2022-06-28 10:34:12 +0900

    plus

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main(int argc, char** argv)
 {
     int select = 0;
+    int a = 0;
+    int b = 0;
 
     printf("1. 더하기 | 2. 빼기 | 3. 곱하기 | 4. 나누기\n");
     printf("> ");
@@ -10,7 +12,9 @@ int main(int argc, char** argv)
 
     switch(select){
         case 1:
-        break;
+            printf("두 정수 입력\n");
+            scanf("%d %d", &a, &b);
+            break;
         case 2:
         break;
         case 3:
