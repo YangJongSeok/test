3309819fd92aebbccec26f005cd5d1c3d66df88d
YangJongSeok <marymet@naver.com>
2022-06-28 09:54:54 +0900

    first

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(int argc, char** argv)
+{
+    int select = 0;
+
+    printf("1. 더하기 | 2. 빼기 | 3. 곱하기 | 4. 나누기\n");
+    printf("> ");
+    scanf("%d", &select);
+
+    switch(select){
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        default:
+            printf("1~4 이내의 숫자를 입력해주세요\n");
+    }
+
+
+    return 0;
+}
